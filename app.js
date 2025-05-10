c7f96ed28ab5b75c2de4afafaf8d5295f3ef22b5
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-05-10 17:48:58 -0300

    feat: interface and seo improvements

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+    res.locals.year = new Date().getFullYear();
+    next();
+});
+
 app.get('/', (req, res) => {
 
     const files = fs.readdirSync(postsDir)
+    const title = 'Home - Money & Code'
+    const description = 'Money & Code: artigos sobre carreira, programação e finanças para transformar conhecimento em liberdade financeira.'
 
     const posts = files
         .filter(file => file !== 'post.json')
@@ -30,41 +37,73 @@ app.get('/', (req, res) => {
         .sort((a, b) => b.id - a.id)
 
     res.render('index', { 
-        posts
+        title,
+        description,
+        posts,
     })
 })
 
 app.get('/sobre', (req, res) => {
-    res.render('about')
+    const title = 'Sobre - Money & Code'
+    const description = 'Saiba mais sobre o Money & Code: uma plataforma que une finanças e tecnologia para ajudar você a melhorar sua vida financeira e profissional.'
+    res.render('about', {
+        title,
+        description,
+    })
 })
 
 app.get('/privacidade', (req, res) => {
-    res.render('privacy')
+    const title = 'Privacidade - Money & Code'
+    const description = 'Veja como protegemos seus dados: nossa política de privacidade explica a coleta, uso e segurança das suas informações pessoais.'
+    res.render('privacy', {
+        title,
+        description,
+    })
 })
 
 app.get('/contato', (req, res) => {
-    res.render('contact')
+    const title = 'Contato - Money & Code'
+    const description = 'Fale com o Money & Code: envie dúvidas, ideias ou propostas e ajude a construir uma plataforma ainda melhor.'
+    res.render('contact', {
+        title,
+        description,
+    })
 })
 
 app.get('/artigo/:slug', (req, res) => {
 
     let slug = req.params.slug
     let file = posts[slug]
-
+    
     if (!file) {
-        res.status(404).render('404')
+        const title = 'Oops - Money & Code'
+        const description = 'Nada encontrado nessa página.'
+        res.status(404).render('404', {
+            title,
+            description,
+        })
         return
     }
-
-    let post = JSON.parse(fs.readFileSync(path.join(postsDir, file), 'utf-8'))
     
+    let post = JSON.parse(fs.readFileSync(path.join(postsDir, file), 'utf-8'))
+
+    const title = `${post.title} - Money & Code`;
+    const description = `Leia o artigo: ${post.title}.`
+
     res.render('post', {
-        post
+        title,
+        description,
+        post,
     })
 })
 
 app.use((req, res) => {
-    res.status(404).render('404')
+    const title = 'Oops - Money & Code'
+    const description = 'Nada encontrado nessa página.'
+    res.status(404).render('404', {
+        title,
+        description,
+    })
 })
 
 app.listen(port, () => {
